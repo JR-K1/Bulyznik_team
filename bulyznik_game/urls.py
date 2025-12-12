6d7419ff5bccdb607fc742dc81c75ce36a26aaee
Ye-Egor <144787573+Ye-Egor@users.noreply.github.com>
2025-12-12 20:09:10 +0500

    левл2

--- bulyznik_game/urls.py
+++ bulyznik_game/urls.py
@@ -5,6 +5,9 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('game.urls')),
-    path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
+    path('accounts/login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+
+
+
 ]
--- game/urls.py
+++ game/urls.py
@@ -3,11 +3,13 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    # Маршруты для уровней
     path('level/<int:level_number>/', views.level_detail, name='level_detail'),
     path('level/<int:level_number>/practice/', views.level_practice, name='level_practice'),
-    path('level/<int:level_number>/test/', views.level_test, name='level_test'),  # ← НОВЫЙ
+    path('level/<int:level_number>/test/', views.level_test, name='level_test'),
+    # Специальный маршрут для 2 уровня
+    path('lab/level2/', views.lab_level_2, name='lab_level_2'),
+    # API маршруты
     path('api/filter-minerals/', views.filter_minerals, name='filter_minerals'),
-    path('api/level/<int:level_number>/submit-test/', views.submit_test, name='submit_test'),  # ← НОВЫЙ
-
-    path('level/2/lab/', views.lab_level_2, name='lab_level_2'),
+    path('api/submit-test/<int:level_number>/', views.submit_test, name='submit_test'),
 ]
